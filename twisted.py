7122bb8f18b3d3d74c21e2d6fac9a7ed48321a46
Kurtis Jungersen <kmjungersen@gmail.com>
2014-05-21 10:16:29 -0400

    Initial Commit

---
+++ twisted.py
@@ -0,0 +1,19 @@
+__author__ = 'kurtisjungersen'
+
+
+from twisted.internet import reactor
+from twisted.internet.protocol import Factory, Protocol
+from  import SockJSFactory
+
+class HelloProtocol(Protocol):
+    def connectionMade(self):
+        self.transport.write('Hellllllooooooooo')
+
+    def dataReceived(self, data):
+        print data
+
+
+reactor.listenTCP(8080, SockJSFactory(Factory.forProtocol(HelloProtocol)))
+reactor.run()
+
+
